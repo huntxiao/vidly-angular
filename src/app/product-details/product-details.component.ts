71df8e51da2768c481d6d49a3aa88a9e9ae16d90
huntxiao <huntxiao@gmail.com>
2019-07-31 00:07:41 +0800

    1111

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
+import { CartService } from "../cart.service";
+interface Product {
+  name: String;
+  price: Number;
+  description: String;
+}
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  product: Product;
+  addToCart(product) {
+    window.alert("Your product has been added to the cart!");
+    this.cartService.addToCart(product);
   }
-
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
+  ngOnInit() {
+    console.log("this.route.paramMap", this.route.paramMap);
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get("productId"));
+      this.product = products[+params.get("productId")];
+    });
+  }
 }
